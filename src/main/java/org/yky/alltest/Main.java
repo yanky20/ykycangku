ae62f27a098f024c6a9e8c027554e333b5fe14f7
严恺阳 <@yankaiyang>
2018-12-05 10:34:38 +0800

    2

--- src/main/java/org/yky/alltest/Main.java
+++ src/main/java/org/yky/alltest/Main.java
@@ -8,13 +8,7 @@ import net.sf.json.JSONObject;
  */
 public class Main {
     public static void main(String[] args){
-        Object1 o1;
-        Object2 o2 = new Object2();
-        o2.setAge(1);
-        o2.setHobby("唱歌");
-        o2.setName("yankaiyang");
-        o2.setSex(2);
-        o1 = (Object1) JSONObject.toBean(JSONObject.fromObject(o2), Object1.class);
-        System.out.println(o1.getAge());
+//        System.out.println(Enum1.A.getValue());
+        System.out.println(111);
     }
 }
