bb590ef6f58c893ce5ba6c15a46a10e6747e384a
Terryyan <terryyan@yeahka.com>
2020-11-04 11:14:01 +0800

    asdasd

--- src/main/java/org/yky/test/suanfa/lianbiao/LianbiaoFanxu.java
+++ src/main/java/org/yky/test/suanfa/lianbiao/LianbiaoFanxu.java
@@ -1,31 +1,15 @@
-package org.yky.alltest;
+package org.yky.test.suanfa.lianbiao;
 
-
-import net.sf.json.JSONObject;
-
-import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-/**
- * Created by lenovo on 2018/12/4.
- */
-public class Main {
-
-
+public class LianbiaoFanxu {
     public static void main(String[] args){
         Node a = new Node(null, 3);
-//        Node b = new Node(a, 2);
-//        Node b1 = new Node(b, 12);
-//        Node b2 = new Node(b1, 22);
-//        Node b3 = new Node(b2, 32);
-//        Node c = new Node(b3, 1);
+        Node b = new Node(a, 2);
+        Node b1 = new Node(b, 12);
+        Node b2 = new Node(b1, 22);
+        Node b3 = new Node(b2, 32);
+        Node c = new Node(b3, 1);
 
-        Node shaobing = new Node(a, 0);
+        Node shaobing = new Node(c, 0);
 
         Node nd = shaobing;
         printNode(nd);
@@ -72,5 +56,4 @@ public class Main {
         Node() {
         }
     }
-
 }
